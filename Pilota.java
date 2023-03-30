b1830a48d2fcac16dcfc7677dd14ff80225aa1a3
Alessandro De Marzo <demarzoad@libero.it>
2023-03-30 16:51:38 +0200

    first commit

---
+++ Pilota.java
@@ -0,0 +1,15 @@
+public class Pilota extends Thread{
+
+    private String name;
+
+    Pilota(String  name){
+        this.name = name;
+    }
+
+    @Override
+    public synchronized void run() {
+
+        
+
+    }
+}
